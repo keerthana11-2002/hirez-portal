32601593503d006de28911b6c52f19ed9f0ec35d
keerthana11-2002 <154956105+keerthana11-2002@users.noreply.github.com>
2024-10-14 11:59:59 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base:"/hirez-portal/",
+  base: process.env.NODE_ENV === 'production' ? '/hirez-portal/' : '/',
 })
